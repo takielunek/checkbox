7d1cb047f4fdc3273af708a6b9bf91ebc6f92c3e
Karolina <karolina-jesionek@wp.pl>
2024-03-05 10:42:52 +0100

    changes

--- src/ReactHookForm.jsx
+++ src/ReactHookForm.jsx
@@ -1,28 +1,47 @@
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 function ReactHookForm() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, setValue, watch, getValues } = useForm();
+  const [checkedAll, setCheckedAll] = useState(false);
 
   const onSubmit = (data) => alert(JSON.stringify(data));
-  const selectAll = watch("selectAll");
-  console.log("selectAll", selectAll);
+
+  const handleSelectAll = () => {
+    setValue("1", !checkedAll);
+    setValue("2", !checkedAll);
+    setCheckedAll(!checkedAll);
+  };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>
         Select all:
-        <input type="checkbox" value="all" {...register("selectAll")} />
+        <input
+          name="selectAll"
+          type="checkbox"
+          onChange={handleSelectAll}
+          checked={checkedAll}
+        />
       </label>
       <br />
       <label>
-        Lower Checkbox:
+        Checkbox_1:
         <input
           type="checkbox"
-          value="lower"
-          checked={selectAll}
-          {...register("lower", {
-            required: true,
-          })}
+          {...register("1", { required: true })}
+          checked={watch("1")}
+          onChange={() => setValue("1", !getValues("1"))}
+        />
+      </label>
+      <br />
+      <label>
+        Checkbox_2:
+        <input
+          type="checkbox"
+          {...register("2", { required: true })}
+          checked={watch("2")}
+          onChange={() => setValue("2", !getValues("2"))}
         />
       </label>
       <br />
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import ReactHookForm from "./ReactHookForm.jsx";
-import './index.css'
-import OnlyJavaScript from "./OnlyJavaScript.jsx";
-
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ReactHookForm />
-    <OnlyJavaScript />
   </React.StrictMode>
 );
